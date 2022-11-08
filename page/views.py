6a005003386d2b4362b12a3cd751925958457bae
JG-C3 <jgc3django@gmail.com>
2022-11-08 19:52:55 +0900

    Update Django W9

--- page/views.py
+++ page/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Posting, Comment
 from .forms import PostingForm, CommentForm
-# [코드 작성] login_required 데코레이션 추가
-
 
 # Create your views here.
 def index(request):
@@ -18,14 +16,10 @@ def posting_list(request):
 
 # [Create] 글 작성하기
 def posting_create(request):
-    # [코드 작성] 웹 페이지에 로그인이 되어있는 경우만 글 쓰기가 가능하도록 조건문 작성
-
     if request.method == 'POST':
         posting_form = PostingForm(request.POST)
         
         if posting_form.is_valid():
-            # [코드 작성] posting_form에 작성자 객체 추가
-
             posting_form.save()
             return redirect('page:posting_list')
     else:
@@ -36,8 +30,6 @@ def posting_create(request):
         'posting_form': posting_form,
     }
     return render(request, 'page/posting_form.html', context)
-    # [코드 작성] 로그인이 되어있지 않은 경우 로그인 페이지로 돌아가도록 처리
-    
 
 # [Read & Create] 작성글 보기 & 댓글 작성
 def posting_detail(request, posting_id):
@@ -47,11 +39,9 @@ def posting_detail(request, posting_id):
         comment_form = CommentForm(request.POST)
 
         if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-            comment.posting = posting
-            # [코드 작성] comment_form에 작성자 객체 추가
-
-            comment.save()
+            comment_form = comment_form.save(commit=False)
+            comment_form.posting = posting
+            comment_form.save()
             return redirect('page:posting_detail', posting_id)
     else :
         comment_form = CommentForm()
@@ -64,14 +54,10 @@ def posting_detail(request, posting_id):
         'comments': comments,
     }
     return render(request, 'page/posting_detail.html', context)
-    
-# [Update] 작성글 수정
-# [코드 작성] login_required 데코레이션 추가
 
+# [Update] 작성글 수정
 def posting_update(request, posting_id):
     posting = get_object_or_404(Posting, id=posting_id)
-    # [코드 작성] 글 작성자가 로그인한 사람과 같을 경우에만 글 수정이 가능하도록 조건문 작성
-    
     if request.method == 'POST':
         posting_form = PostingForm(request.POST, instance=posting)
 
@@ -87,12 +73,8 @@ def posting_update(request, posting_id):
         'posting_form': posting_form,
     }
     return render(request, 'page/posting_form.html', context)
-    # [코드 추가] posting_id에 해당하는 페이지로 redirect
-
 
 # [Delete] 작성글 삭제
-# [코드 작성] login_required 데코레이션 추가
-
 def posting_delete(request, posting_id):
     if request.method == 'POST':
         posting = get_object_or_404(Posting, id=posting_id)
@@ -101,8 +83,6 @@ def posting_delete(request, posting_id):
     return redirect('page:posting_detail', posting_id)
 
 # [Delete] 댓글 삭제
-# [코드 작성] login_required 데코레이션 추가
-
 def comment_delete(request, posting_id, comment_id):
     if request.method == 'POST':
         comment = get_object_or_404(Comment, id=comment_id)
